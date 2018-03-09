12b278effb700c568efa46935e38c2d7cb71472c
Wayde <waydemlyle@gmail.com>
2018-03-09 13:13:38 +0200

    Added config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+/**
+ * Config
+ */
+const config = {
+  "ROOT_API_ENDPOINT" : "https://sandbox.root.co.za/v1/insurance",
+  "AUTHORIZATION" : "Basic c2FuZGJveF9PV1k1T1RBMU1URXRPVE5pWmkwME5XSTRMV0UyTkRjdE5XWm1aV1l5TkdFMk1EQXhMbXRSTFd4NlRqYzRWV0ZmYzJkTFFrWkxTMm81UkdaSVJrNTFXVzloYkc5SDo="
+};
+
+module.exports = config;
