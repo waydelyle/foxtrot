e3bf2258d05a2894cc7921b9b6e0223484deb1cc
Wayde <waydemlyle@gmail.com>
2018-03-09 11:35:13 +0200

    Fixed root insurance call.

--- src/controllers/InsuranceQuote.js
+++ src/controllers/InsuranceQuote.js
@@ -3,9 +3,7 @@
  * @type {string}
  */
 const ROOT_API_ENDPOINT = 'https://sandbox.root.co.za/v1/insurance';
-const CLIENT_ID = '42b7212c-1e38-11e8-a633-5b3f08f8ae37';
-const CLIENT_SECRET = 'sandbox_ZDU0NzNiODctNzFmNC00NTk4LTgxZjktMGZhNmU1OTQ2MjQ1LndFQjFGRGd5cUNYdjhfTU8wdUplNE1ZNmRLY0VOSG50';
-const AUTH_TOKEN = "Basic " + new Buffer(CLIENT_ID + ":" + CLIENT_SECRET).toString("base64");
+const AUTHORIZATION = "Basic c2FuZGJveF9PV1k1T1RBMU1URXRPVE5pWmkwME5XSTRMV0UyTkRjdE5XWm1aV1l5TkdFMk1EQXhMbXRSTFd4NlRqYzRWV0ZmYzJkTFFrWkxTMm81UkdaSVJrNTFXVzloYkc5SDo=";
 
 const request = require('request-promise');
 
@@ -27,9 +25,7 @@ const InsuranceQuote = {
             const options = {
                 method: 'POST',
                 headers: {
-                    username: CLIENT_ID,
-                    password: CLIENT_SECRET,
-                    authorization: AUTH_TOKEN
+                    Authorization: AUTHORIZATION,
                 },
                 uri: reqUrl,
                 form: {
@@ -40,6 +36,7 @@ const InsuranceQuote = {
             };
 
             request(options, function(error, response, body) {
+                res.send(response);
                 if (error) {
                     res.send(error);
                 }
