7cd02d62be7cdc4b0c0e32570ea9ebb3ec95a31b
Wayde <waydemlyle@gmail.com>
2018-03-09 19:08:16 +0200

    Removed key.

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
  */
 const config = {
   "ROOT_API_ENDPOINT" : "https://sandbox.root.co.za/v1/insurance",
-  "AUTHORIZATION" : "Basic c2FuZGJveF9PV1k1T1RBMU1URXRPVE5pWmkwME5XSTRMV0UyTkRjdE5XWm1aV1l5TkdFMk1EQXhMbXRSTFd4NlRqYzRWV0ZmYzJkTFFrWkxTMm81UkdaSVJrNTFXVzloYkc5SDo="
+  "AUTHORIZATION" : "Basic KEY-HERE"
 };
 
 module.exports = config;
